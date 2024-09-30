fe429e4d81da239ecf55b0e6b1f587ff74860ee6
lelien0205 <lethiphuonglien0205@gmail.com>
2024-10-01 00:50:13 +0700

    bai17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,8 +4,15 @@
 import React from "react";
 class MyComponent extends React.Component {
   // JSX
+  state = {
+    name: "Lien",
+    address: "Thanh Hoa",
+    age: 23,
+  };
+
+  //JSX
   render() {
-    return <div>my first component</div>;
+    return <div>my name is {this.state.name}</div>;
   }
 }
 
