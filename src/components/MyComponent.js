97593b75b1288bf93c566af74e7c487b0a2a0b70
lelien0205 <lethiphuonglien0205@gmail.com>
2024-10-02 00:30:24 +0700

    bai19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,9 +10,42 @@ class MyComponent extends React.Component {
     age: 23,
   };
 
+  handleClick = (event) => {
+    console.log(">> click me my button");
+    //console.log("My name is", this.state.name);
+    // console.log("random", Math.floor(Math.random() * 100));
+
+    // merge state => react class
+    this.setState({
+      name: "LeLien",
+      age: Math.floor(Math.random() * 100),
+    });
+
+    // this.setState({
+    //   age:  Math.floor(Math.random() * 100);
+    // })
+  };
+
+  handleOnMoverOver(event) {
+    console.log(event);
+  }
+
   //JSX
   render() {
-    return <div>my name is {this.state.name}</div>;
+    return (
+      <div>
+        my name is {this.state.name} and {this.state.age}
+        {/* <button onClick={this.handleClick}>Click me</button> */}
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me
+        </button>
+        <button onMouseOver={this.handleOnMoverOver}> Hover me</button>
+      </div>
+    );
   }
 }
 
