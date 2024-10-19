564c898133d37b4279f27cfb17becad55268db50
lelien0205 <lethiphuonglien0205@gmail.com>
2024-10-19 15:47:41 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.scss";
+import Header from "./components/header/Header";
+import { Outlet, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      hrirhvolkn
-      <button>test</button>
+    <div className="App-container">
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="main-container">
+        <div className="sidenar-container"></div>
+        <div className="app-content">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/header/Header.js
@@ -0,0 +1,38 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">Hoi dan IT</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/user" className="nav-link">
+              User
+            </NavLink>
+            <NavLink to="/admin" className="nav-link">
+              Admin
+            </NavLink>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Settings" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#log_in">Log in</NavDropdown.Item>
+              <NavDropdown.Item href="#log_out">Log out</NavDropdown.Item>
+              <NavDropdown.Item href="#log_out">Profile</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
